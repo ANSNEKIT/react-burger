b4a3f8ef0907f8f53fedec8bc89d45476f8700b0
Amelchenko Nikita <amelchenko95@yandex.ru>
2025-09-28 19:23:19 +0300

    add ingredient page

---
+++ src/pages/ingredient-page/ingredient-page.tsx
@@ -0,0 +1,32 @@
+import { IngredientDetails } from '@/components/ingredient-details/ingredient-details';
+
+import type { TIngredientDTO } from '@/contracts/ingredientDTO';
+import type { ReactElement } from 'react';
+
+import styles from './ingredient-page.module.css';
+
+const IngredientPage = (): ReactElement => {
+  const currentIngredient: TIngredientDTO = {
+    _id: '643d69a5c3f7b9001cfa0941',
+    name: 'Биокотлета из марсианской Магнолии',
+    type: 'main',
+    proteins: 420,
+    fat: 142,
+    carbohydrates: 242,
+    calories: 4242,
+    price: 424,
+    image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png',
+    __v: 0,
+  };
+
+  return (
+    <div className={`page ${styles.ingredientPage}`}>
+      <h2 className="text text_type_main-large">Детали ингредиента</h2>
+      <IngredientDetails detail={currentIngredient} />
+    </div>
+  );
+};
+
+export default IngredientPage;
