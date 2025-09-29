31feef70b29c60985bd5c1498a59439530ae21fe
Amelchenko Nikita <amelchenko95@yandex.ru>
2025-09-29 16:20:32 +0300

    add constructor page

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,46 +1,16 @@
-import { useAppDispatch } from '@/services/hooks';
-import { loadIngredients } from '@/services/ingredients/actions';
-import { getIngredientsState } from '@/services/ingredients/selectors';
-import { useEffect } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useSelector } from 'react-redux';
+import ConstructorPage from '@/pages/constructor-page/constructor-page';
 
 import { AppHeader } from '../app-header/app-header';
-import { BurgerConstructor } from '../burger-contructor/burger-constructor';
-import { BurgerIngredients } from '../burger-ingredients/burger-ingredients';
+
+import type { ReactElement } from 'react';
 
 import styles from './app.module.css';
 
-export const App = (): React.JSX.Element => {
-  const dispatch = useAppDispatch();
-
-  const { isLoading, error } = useSelector(getIngredientsState);
-
-  useEffect(() => {
-    void dispatch(loadIngredients());
-  }, [dispatch]);
-
-  if (isLoading) {
-    return <div>Загрузка</div>;
-  }
-
-  if (!isLoading && error) {
-    return <h2>{`Ошибка: ${error}`}</h2>;
-  }
-
+export const App = (): ReactElement => {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
-        Соберите бургер
-      </h1>
-      <main className={`${styles.main} pl-5 pr-5`}>
-        <DndProvider backend={HTML5Backend}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </DndProvider>
-      </main>
+      <ConstructorPage />
     </div>
   );
 };
---
+++ src/pages/constructor-page/constructor-page.tsx
@@ -0,0 +1,45 @@
+import { BurgerConstructor } from '@/components/burger-contructor/burger-constructor';
+import { BurgerIngredients } from '@/components/burger-ingredients/burger-ingredients';
+import { useAppDispatch } from '@/services/hooks';
+import { loadIngredients } from '@/services/ingredients/actions';
+import { getIngredientsState } from '@/services/ingredients/selectors';
+import { type ReactElement, useEffect } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useSelector } from 'react-redux';
+
+import styles from './constructor-page.module.css';
+
+const ConstructorPage = (): ReactElement => {
+  const dispatch = useAppDispatch();
+
+  const { isLoading, error } = useSelector(getIngredientsState);
+
+  useEffect(() => {
+    void dispatch(loadIngredients());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Загрузка</div>;
+  }
+
+  if (!isLoading && error) {
+    return <h2>{`Ошибка: ${error}`}</h2>;
+  }
+
+  return (
+    <div className="page flex-column">
+      <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
+        Соберите бургер
+      </h1>
+      <main className={`${styles.main} pl-5 pr-5`}>
+        <DndProvider backend={HTML5Backend}>
+          <BurgerIngredients />
+          <BurgerConstructor />
+        </DndProvider>
+      </main>
+    </div>
+  );
+};
+
+export default ConstructorPage;
