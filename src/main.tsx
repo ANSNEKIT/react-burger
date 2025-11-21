ee284478f3d50055ffd08ecaf6a444de9b9c9aad
Amelchenko Nikita <amelchenko95@yandex.ru>
2025-11-21 18:43:09 +0300

    fix router for production

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 
 import { store } from './services/store';
 import { App } from '@components/app/app';
@@ -10,10 +10,10 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
