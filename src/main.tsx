f46ff26bbf2fb71f9d7d728cee41efdac222442c
Amelchenko Nikita <amelchenko95@yandex.ru>
2025-11-21 19:45:10 +0300

    fix logic router

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,31 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router';
+import { BrowserRouter, HashRouter } from 'react-router';
 
 import { store } from './services/store';
 import { App } from '@components/app/app';
 
 import './index.css';
 
+const isProdMode = import.meta.env.MODE === 'production';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </HashRouter>
+    {isProdMode && (
+      <HashRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </HashRouter>
+    )}
+
+    {!isProdMode && (
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    )}
   </StrictMode>
 );
